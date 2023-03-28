8642f7c035868cb56c414f720c47bb10bf8fdccc
tomast1337 <vycasnicolas@gmail.com>
2023-03-28 00:36:16 -0300

    Semana 03

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 const scripts = {
-    "clean": "rm -rf dist",
-    "start": "npm run clean && tsc && node dist/index.js",
-    "singleton": "npm run clean && tsc && node dist/singleton-pattern/index.js",
-    "command": "npm run clean && tsc && node dist/command-pattern/index.js",
-    "facade": "npm run clean && tsc && node dist/facade-pattern/index.js",
-    "dao": "npm run clean && tsc && node dist/dao-pattern/index.js",
-    "factory-method": "npm run clean && tsc && node dist/factory-method-pattern/index.js",
-    "abstract-factory": "npm run clean && tsc && node dist/abstract-factory-pattern/index.js"
-}
+  clean: "rm -rf dist",
+  start: "npm run clean && tsc && node dist/index.js",
+  singleton: "npm run clean && tsc && node dist/singleton-pattern/index.js",
+  command: "npm run clean && tsc && node dist/command-pattern/index.js",
+  facade: "npm run clean && tsc && node dist/facade-pattern/index.js",
+  dao: "npm run clean && tsc && node dist/dao-pattern/index.js",
+  "factory-method":
+    "npm run clean && tsc && node dist/factory-method-pattern/index.js",
+  "abstract-factory":
+    "npm run clean && tsc && node dist/abstract-factory-pattern/index.js",
+  "decorator-pattern":
+    "npm run clean && tsc && node dist/decorator-pattern/index.js",
+  "observer-pattern":
+    "npm run clean && tsc && node dist/observer-pattern/index.js",
+  "state-pattern": "npm run clean && tsc && node dist/state-pattern/index.js",
+  "strategy-pattern":
+    "npm run clean && tsc && node dist/strategy-pattern/index.js",
+};
 
 // print the available scripts
 console.log(scripts);
